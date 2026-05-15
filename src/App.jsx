7dd813f66e325b24c9ebbf5c4b6013928c7f51e5
Ajay Kumar <ajaykumar@Ajays-MacBook-Air.local>
2026-05-15 11:46:31 -0400

    initial project and folder structure setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+  return (
+    <div className="p-8 font-mono text-gray-700">
+      <h1 className="text-2xl font-bold">Nested Form Builder</h1>
+    </div>
+  )
+}
